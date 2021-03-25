2b83395e13b9e086ed1e043e7fa4fcaa506dc327
Tairah <Tairah.akintoye@ocsbstudent.ca>
2021-03-25 03:13:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let age = 0
+
+document.getElementById('button').addEventListener('click', check)
+
+function check () {
+age = document.getElementById('age').value
+age = parseInt(age)
+
+if (age > 1 ) {
+  alert('You can watch a R rated movie alone.')
+
+} else if (age >= 13) {
+  alert('You can see a PG-13 movie by yourself.')
+
+} else if (age >= 5) {
+  alert('You can see a G or PG movie alone.')
+
+} else {
+  alert('You are too young to see all movies.')
+  }
+}
